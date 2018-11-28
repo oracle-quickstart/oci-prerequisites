db8f04923242b2a4cd03b5bc13a43c09a6a036e2
cpoczatek <collin.poczatek@gmail.com>
2018-11-27 16:20:26 -0800

    undo default region change

--- env-vars.sh
+++ env-vars.sh
@@ -7,7 +7,7 @@ export TF_VAR_tenancy_ocid="<tenancy OCID>"
 export TF_VAR_user_ocid="<user OCID>"
 export TF_VAR_fingerprint=$(cat ~/.oci/oci_api_key.fingerprint)
 export TF_VAR_private_key_path="~/.oci/oci_api_key.pem"
-export TF_VAR_region="us-phoenix-1"
+export TF_VAR_region="us-ashburn-1"
 
 # Keys used to SSH to OCI VMs
 export TF_VAR_ssh_public_key=$(cat ~/.ssh/oci.pub)
